d64ac594f18b50041c9f48f9d482fbbb2158e68f
gendeld <gendeld@gmail.com>
2017-09-16 15:27:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,44 +12,69 @@ import {
 var CollectionView = React.createClass({
   groupItems: function (items, itemsPerRow) {
     var itemsGroups = [];
-    var group = [];
-    items.forEach(function (item) {
-      if (group.length === itemsPerRow) {
-        itemsGroups.push(group);
-        group = [item];
-      } else {
-        group.push(item);
-      }
-    });
-
-    if (group.length > 0) {
-      itemsGroups.push(group);
-    }
-
-    return itemsGroups;
+        var group = [];
+        var flag=false;
+        items.forEach(function(item) {
+          if(flag)
+          {
+            if (group.length === itemsPerRow) {
+              itemsGroups.push(group);
+              group = [item];
+            } else {
+              group.push(item);
+            }
+          }
+          else {
+            flag=true;
+          }
+        });
+        if (group.length > 0) {
+          itemsGroups.push(group);
+        }
+        itemsGroups.unshift([items[0]]);
+        return itemsGroups;
   },
   renderGroup: function ({ item }) {
     var that = this;
-    var items = item.map(function (obj, index) {
-      return that.props.renderItem(obj, index);
-    });
-    items = items.map((obj, i) => {
+    if(item.length==1&&this.state.flag)
+    {
+      this.state.flag=false;
+      var items = item.map(function (obj, index) {
+        return that.props.renderHeader(obj, that.props.openModal, that.props.mCon);
+      });
       return (
-        <View style={{ flex: 1 }}>
-          {obj}
-        </View>)
+        <View>
+          {
+            items
+          }
+          {this.getBlankCell(item.length, this.props.itemsPerRow)
+          }
 
-    })
-    return (
-      <View style={styles.group}>
-        {
-          items
-        }
-        {this.getBlankCell(item.length, this.props.itemsPerRow)
-        }
+        </View>
+      );
+    }
+    else {
+      var items = item.map(function (obj, index) {
+        return that.props.renderItem(obj, that.props.openModal, index, that.props.mCon);
+      });
+      items = items.map((obj, i) => {
+        return (
+          <View style={{ flex: 1 }}>
+            {obj}
+          </View>)
 
-      </View>
-    );
+      })
+      return (
+        <View style={styles.group}>
+          {
+            items
+          }
+          {this.getBlankCell(item.length, this.props.itemsPerRow)
+          }
+
+        </View>
+      );
+    }
   },
 
   getBlankCell(currentItems, itemsPerRow) {
@@ -67,6 +92,7 @@ var CollectionView = React.createClass({
 
   },
   render: function () {
+    this.state={flag: true};
     var groups = this.groupItems(this.props.items, this.props.itemsPerRow);
     return (<FlatList
       data={groups}
@@ -79,10 +105,12 @@ var CollectionView = React.createClass({
 
 var styles = StyleSheet.create({
   group: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
     justifyContent: 'center',
-    overflow: 'hidden'
+    overflow: 'hidden',
+    paddingHorizontal: 25,
+    paddingBottom: 8
   }
 });
 
